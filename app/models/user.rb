308e4e9f0e13ff7a2e39e52dc3af5be4dd03f7a8
ws903 <ws903@nyu.edu>
2018-10-23 16:08:04 -0400

    more exception handlings

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,15 +9,16 @@ class User < ActiveRecord::Base
 
 	def make_transaction(ticker_name:)
 		puts "How many shares?"
-		quantity_shares = gets.chomp.to_i
-		if quantity_shares > 0 
-		Stock.get_stock_price(ticker_name: ticker_name)
-		stock = Stock.find_by(ticker_name: ticker_name)
-		transaction = Transaction.create(quantity_shares: quantity_shares, stock_price: stock.stock_price, stock_id: stock.id, user_id: self.id)
-		puts "Success! You just bougth #{transaction.quantity_shares} #{ticker_name} shares!"
-		self.update_balance
-	else
-		puts "You are trying to make invalid transaction!"
+		quantity_shares = gets.chomp
+		if !!/\A\d+\z/.match(quantity_shares)
+			quantity_share = quantity_share.to_i
+			Stock.get_stock_price(ticker_name: ticker_name)
+			stock = Stock.find_by(ticker_name: ticker_name)
+			transaction = Transaction.create(quantity_shares: quantity_shares, stock_price: stock.stock_price, stock_id: stock.id, user_id: self.id)
+			puts "Success! You just bougth #{transaction.quantity_shares} #{ticker_name} shares!"
+			self.update_balance
+		else
+			puts "You are trying to make invalid transaction!"
 		end
 	end
 
@@ -52,13 +53,13 @@ class User < ActiveRecord::Base
 	end
 
 	def sell_n_ticker_shares(ticker_name:, sell_quantity:)
-		transaction = find_transaction(ticker_name: ticker_name)
-		updated_price = Stock.get_stock_price(ticker_name: ticker_name)
-		if sell_quantity.to_i <= transaction.quantity_shares && sell_quantity.to_i > 0
-		self.balance -= (updated_price * sell_quantity.to_f).round(2)
-		transaction.quantity_shares -= sell_quantity.to_i
-	else
-		puts "You are trying to make invalid transaction!"
+		if sell_quantity.to_i <= transaction.quantity_shares && !!/\A\d+\z/.match(sell_quantity)
+			transaction = find_transaction(ticker_name: ticker_name)
+			updated_price = Stock.get_stock_price(ticker_name: ticker_name)
+			self.balance -= (updated_price * sell_quantity.to_i).round(2)
+			transaction.quantity_shares -= sell_quantity.to_i
+		else
+			puts "You are trying to make invalid transaction!"
 	end
 end
 
